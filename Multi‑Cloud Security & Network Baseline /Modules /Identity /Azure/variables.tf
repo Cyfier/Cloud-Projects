1b1d763c66e197ac558825efe6f71027d6b62ade
Jonathan <jburgher00@gmail.com>
2026-02-16 15:26:55 -0500

    Add files via upload

---
+++ Multi‑Cloud Security & Network Baseline /Modules /Identity /Azure/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "sso_instance_arn" {
+  type        = string
+  description = "AWS IAM Identity Center instance ARN"
+}
