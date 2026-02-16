a895e6914352793434d6451a9384d23455eceaf8
Jonathan <jburgher00@gmail.com>
2026-02-16 15:24:33 -0500

    .

--- Multi‑Cloud Security & Network Baseline /Modules /Identity /AWS/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
-}
-
-variable "sso_instance_arn" {
-  type        = string
-  description = "AWS IAM Identity Center instance ARN"
-}
